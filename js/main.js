2b1774f72e9b17ba910f4e7468e450426d78f2c6
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2019-05-02 16:38:29 -0400

    add css switch. add javascript to add emoji div elements. add jquery to switch to toggle display none between cicrles and emojis

--- js/main.js
+++ js/main.js
@@ -1,39 +1,41 @@
-var x;
-var y;
-var a = 0;
+var colors = ["rgb(218, 88, 88)", "rgb(146, 108, 207)", "rgb(233, 188, 71)", "rgb(250, 247, 1)", "rgb(167, 115, 252)", "rgb(189, 210, 114)", "rgb(110, 175, 181)", "rgb(110, 210, 228)", "rgb(1, 122, 69)", "rgb(208, 79, 31)", "rgb(41, 169, 186)"
+, "rgb(149, 28, 180)", "rgb(240, 161, 168)", "rgb(255, 232, 133)"];
 
-var d = document.getElementsByClassName('shape');
+var emojis = ['💩', '✌️', '👻', '⚡️', '🌻', '🤙', '👽', '🍕', '🏝', '☀️', '🥑', '🔮', '🌎'];
 
-function pop() {
+function addBubble() {
 
-  if (a == 13)
-    removeEventListener('click', pop);
+  $(".container").append('<div class="shape animated pulse infinite"></div><div class="emoji animated pulse infinite"></div>');
+  var circle = $(".shape").last();
+  var emojiObject = $(".emoji").last();
+
+  var randomEmojiNum = Math.random() * (emojis.length);
+  var randomColorNum = Math.random() * (colors.length);
+
+  randomEmojiNum = Math.floor(randomEmojiNum);
+  randomColorNum = Math.floor(randomColorNum);
+
+  console.log("random Emoji Num = " + randomEmojiNum);
+  console.log("random Color Num = " + randomColorNum);
 
 
-  else {
-    d[a].style.left = event.x - 50 + "px";
-    d[a].style.top = event.y - 50 + "px";
-    d[a].classList.remove('off');
-    a += 1;
-    console.log(a);
-  }
+  circle[0].style.left = event.x - 50 + "px";
+  circle[0].style.top = event.y - 50 + "px";
+  circle[0].style.backgroundColor = colors[randomColorNum];
+
+  emojiObject[0].style.left = event.x - 50 + "px";
+  emojiObject[0].style.top = event.y - 50 + "px";
+  emojiObject[0].innerHTML = emojis[randomEmojiNum];
 
 }
 
-// function move() {
-//
-//   if (a == 13)
-//     removeEventListener('mousemove', move);
-//
-//
-//   else if (a > 0) {
-//     d[a].style.left = (event.x - 50)/10 + "px";
-//     d[a].style.top = (event.y - 50)/5 + "px";
-//     d[a].classList.remove('off');
-//     console.log(a);
-//   }
-//
-// }
+// 'stopPropagation()' stops the click event from working on top of the switch. This way the pop-up objects do not cover the switch and interfere with the functionality.
+$('.controls').click(function(e) {
+    e.stopPropagation();
+});
 
-addEventListener('click', pop);
-// addEventListener('mousemove', move);
+$('.switchContainer').click(function(e) {
+  $('.shape').css({"display: none"});
+});
+
+addEventListener('click', addBubble);
